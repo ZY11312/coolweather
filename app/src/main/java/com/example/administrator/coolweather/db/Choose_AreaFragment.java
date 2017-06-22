ffc38a85dac83bac4b16c96a9347c2674a4c2554
zyf <zyf@gmail.com>
2017-06-22 14:08:18 +0800

    新增切换城市和手动更新天气的功能

--- app/src/main/java/com/example/administrator/coolweather/db/Choose_AreaFragment.java
+++ app/src/main/java/com/example/administrator/coolweather/db/Choose_AreaFragment.java
@@ -17,6 +17,7 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.administrator.coolweather.MainActivity;
 import com.example.administrator.coolweather.R;
 import com.example.administrator.coolweather.WeatherActivity;
 import com.example.administrator.coolweather.util.HttpUtil;
@@ -84,10 +85,17 @@ public class Choose_AreaFragment extends Fragment {
                     queryCounties();
                 }else if (currentLevel==LEVEL_COUNTY){
                     String weatherId=countyList.get(position).getWeatherId();
-                    Intent intent=new Intent(getActivity(), WeatherActivity.class);
-                    intent.putExtra("weather_id",weatherId);
-                    startActivity(intent);
-                    getActivity().finish();
+                    if (getActivity() instanceof MainActivity){
+                        Intent intent=new Intent(getActivity(), WeatherActivity.class);
+                        intent.putExtra("weather_id",weatherId);
+                        startActivity(intent);
+                        getActivity().finish();
+                    }else if (getActivity() instanceof WeatherActivity){
+                        WeatherActivity activity= (WeatherActivity) getActivity();
+                        activity.drawerLayout.closeDrawers();
+                        activity.swipeRefresh.setRefreshing(true);
+                        activity.requestWeather(weatherId);
+                    }
                 }
             }
         });
